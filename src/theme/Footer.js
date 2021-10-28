c6dc40c274b86a6a19d571dd3e4f1acac7c2ca61
Tim Post <tinkertim@gmail.com>
2021-10-28 09:20:45 -0400

    A hack so dirty, I might just need a shower.

--- src/theme/Footer.js
+++ src/theme/Footer.js
@@ -1,10 +1,42 @@
 import OriginalFooter from '@theme-original/Footer';
 import React from 'react';
+import useIsBrowser from '@docusaurus/useIsBrowser';
+
+export function HeapAnalytics() {
+    if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
+        return null;
+    }
+    window.heap = window.heap || [], heap.load = function(e, t) {
+        window.heap.appid = e, window.heap.config = t = t || {};
+        var r = document.createElement("script");
+        r.type = "text/javascript", r.async = !0, r.src = "https://cdn.heapanalytics.com/js/heap-" + e + ".js";
+        var a = document.getElementsByTagName("script")[0];
+        a.parentNode.insertBefore(r, a);
+        for (var n = function(e) {
+                return function() {
+                    heap.push([e].concat(Array.prototype.slice.call(arguments, 0)))
+                }
+            }, p = ["addEventProperties", 
+                    "addUserProperties", 
+                    "clearEventProperties", 
+                    "identify", 
+                    "resetIdentity", 
+                    "removeEventProperty", 
+                    "setEventProperties", 
+                    "track", 
+                    "unsetEventProperty"
+                ], o = 0; o < p.length; o++) heap[p[o]] = n(p[o])
+    };
+    heap.load("2760903549");
+    return null;
+}
 
 export default function Footer(props) {
-  return (
-    <>
-      <OriginalFooter {...props} />
-    </>
-  );
+    const isBrowser = useIsBrowser();
+    return (
+        <>
+        <OriginalFooter {...props} />
+        <HeapAnalytics />
+        </>
+    );
 }
