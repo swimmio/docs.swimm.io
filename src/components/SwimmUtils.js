823cb0126dbd26383758a6a2ba89e6032da3134a
Tim Post <tim@swimm.io>
2021-10-31 01:02:55 -0400

    More functionality in SwimmUtils

--- src/components/SwimmUtils.js
+++ src/components/SwimmUtils.js
@@ -1,36 +1,56 @@
-import PropTypes from 'prop-types'
 import React from 'react';
+import PropTypes from 'prop-types'
 import {SiteSettings} from '../../swimm.config.js';
-import styles from './SwimmUtils.module.css';
+import GetCurrentSwimmRelease, {GetSpecificSwimmRelease} from '../../swimm.versions.config.js';
 import Link from '@docusaurus/Link';
+import Emojione from 'react-emoji-render';
+
+import styles from './SwimmUtils.module.css';
 
 function Swimm(props) {
-    return null;
+    var index = props || 'version';
+    return SiteSettings[index];
+}
+
+Swimm.propTypes = {
+    index: PropTypes.string
+}
+
+Swimm.defaultProps = {
+    index: 'version'
 }
 
 function SwimmVersion(props) {
-    switch (props.semVer) {
+    var index = props.segment || 'full';
+    switch (index) {
         case 'major':
             return SiteSettings.version.major;
         case 'minor':
             return SiteSettings.version.minor;
         case 'patch':
             return SiteSettings.version.patch;
-        case 'codename':
-            return SiteSettings.version.codename;
-        default:
+        case 'full':
             return SiteSettings.version.major + '.' 
                 + SiteSettings.version.minor + '.' 
-                + SiteSettings.version.patch + 
-                SiteSettings.version.codename ? SiteSettings.version.codename : '';
+                + SiteSettings.version.patch;
+        default:
+            return null;
     }
+}
+
+SwimmVersion.propTypes = {
+    segment: PropTypes.string,
+}
+
+SwimmVersion.defaultProps = {
+    segment: 'full'
 }
 
 function SwimmLink(props) {
     switch (props.target) {
         case 'slack':
             return (
-                <Link to={SiteSettings.community.slack}>{props.text ? props.text : 'office hours'}</Link>
+                <Link to={SiteSettings.community.slack}>{props.text ? props.text : 'Community Slack Channel'}</Link>
             );
         case 'officeHours':
             return (
@@ -41,20 +61,69 @@ function SwimmLink(props) {
     }   
 }
 
-Swimm.propTypes = {
-    emoji: PropTypes.string,
-}
-SwimmVersion.propTypes = {
-    semVer: PropTypes.string,
-}
-
 SwimmLink.propTypes = {
     target: PropTypes.string,
+    text: PropTypes.string
+}
+
+SwimmLink.defaultProps = {
+    target: ''
+}
+
+function SwimmMoji(props) {
+    var slug = props.text;
+    var useSvg = props.useSvg;
+    var emojiConfig = SiteSettings.emojiShortcuts;
+
+    if (typeof(emojiConfig[slug]) != "undefined")
+        var text = emojiConfig[slug];
+    else
+        var text = slug;
+    switch (useSvg) {
+        case true:
+            return(<Emojione svg text={text} />);
+        default:
+            return(<Emojione text={text} />);
+    }
+}
+
+SwimmMoji.propTypes = {
     text: PropTypes.string,
+    svg: PropTypes.bool,
+}
+
+SwimmMoji.defaultProps = {
+    text: SiteSettings.emojiShortcuts.default,
+    svg: true
+}
+
+function SwimmReleaseBlogPost(props) {
+    var releaseInfo = GetSpecificSwimmRelease(props.version);
+    var blogPost = releaseInfo.blog;
+
+    if (blogPost === null)
+        return null;
+    else
+        return (
+            <>
+            <p><b>We have an even more detailed post about this release <Link href={blogPost}>on our main blog</Link>, 
+            which we really hope you enjoy!</b></p>
+            </>
+        );
+}
+
+SwimmReleaseBlogPost.propTypes = {
+    version: PropTypes.string
+}
+
+SwimmReleaseBlogPost.defaultProps = {
+    version: GetCurrentSwimmRelease(),
 }
 
 export {
     Swimm as default,
     SwimmVersion,
     SwimmLink,
+    SwimmMoji,
+    SwimmReleaseBlogPost,
 }
