0fe87d842f2fee9b1c636e07bd903df16e5fc363
Tim Post <tinkertim@gmail.com>
2021-11-04 16:18:50 -0400

    Update version config

--- swimm.versions.config.js
+++ swimm.versions.config.js
@@ -1,9 +1,9 @@
 const SwimmVersions = {
     current: '0.6.4',
-    '0.6.4': {major: 0,minor: 6,patch: 4,blog: null,tweet: null,linkedin: null},
-    '0.6.3': {major: 0,minor: 6,patch: 3,blog: null,tweet: null,linkedin: null},
-    '0.6.2': {major: 0,minor: 6,patch: 2,blog: null,tweet: null,linkedin: null},
-    '0.4.4': {major: 0,minor: 4,patch: 4,blog: 'https://swimm.io/blog/release-notes-dive-into-0-4-4/',tweet: '1390180301993910278',linkedin: null},
+    '0.6.4': {name: '0.6.4', major: 0,minor: 6,patch: 4,blog: null,tweet: null,linkedin: null, youtube: null},
+    '0.6.3': {name: '0.6.3', major: 0,minor: 6,patch: 3,blog: null,tweet: null,linkedin: null, youtube: null},
+    '0.6.2': {name: '0.6.2', major: 0,minor: 6,patch: 2,blog: null,tweet: null,linkedin: null, youtube: null},
+    '0.4.4': {name: '0.4.4', major: 0,minor: 4,patch: 4,blog: 'https://swimm.io/blog/release-notes-dive-into-0-4-4/',tweet: '1390180301993910278',linkedin: null, youtube: 'DX2BGvRbjQE'},
 }
 
 export {SwimmVersions}
